3dbaac23cb0173b41bb0903252287f0e36aab915
Priyanshu8494 <priyanshukumar.8394pk@gmail.com>
2025-04-01 00:06:07 +0530

    setup-new-pc.ps1

--- setup-new-pc.ps1
+++ setup-new-pc.ps1
@@ -1,31 +1,48 @@
 Clear-Host
 
+# Center the PowerShell window
+$hwnd = (Get-Process -Id $pid).MainWindowHandle
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindowAsync(IntPtr hwnd, int nCmdShow);
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    [DllImport("user32.dll")]
+    public static extern bool SetWindowPos(IntPtr hwnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+}
+"@
+[Window]::SetWindowPos($hwnd, [IntPtr]::Zero, [System.Windows.Forms.Screen]::AllScreens[0].Bounds.Width / 4, [System.Windows.Forms.Screen]::AllScreens[0].Bounds.Height / 4, 0, 0, 0)
+
 function Show-Menu {
     param (
         [string]$Message = ""
     )
-    
+
     if ($Message -ne "") {
         Write-Host "`n$Message`n" -ForegroundColor Yellow
     }
-    
+
     Write-Host "      _==/--" -ForegroundColor Yellow
     Write-Host "     /##/     |\_/|     \##\" -ForegroundColor Yellow
     Write-Host "    |####|    (o.o)    |####|" -ForegroundColor Yellow
     Write-Host "     \##\     > ^ <     /##/" -ForegroundColor Yellow
     Write-Host "      '--'               '--'" -ForegroundColor Yellow
-    
+
     Write-Host "============================================" -ForegroundColor Cyan
     Write-Host "          PC SETUP [ PRIYANSHU SURYAVANSHI ]           " -ForegroundColor Green
     Write-Host "============================================" -ForegroundColor Cyan
-    
+
     Write-Host "[1] Install Normal Software (Chrome, WinRAR, VLC, Firefox, Adobe Reader, AnyDesk, UltraViewer)" -ForegroundColor White
     Write-Host "[2] Install MS Office (via GitHub)" -ForegroundColor White
     Write-Host "[3] Activation Tool (Using Custom URL)" -ForegroundColor White
     Write-Host "[0] Exit" -ForegroundColor Red
     Write-Host "============================================" -ForegroundColor Cyan
-    
-    $choice = Read-Host "Choose an option [1,2,3,0]"
+
+    $Key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    $choice = $Key.Character
 
     switch ($choice) {
         '1' {
