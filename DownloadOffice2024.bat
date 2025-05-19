ee084a3d7fdefb6fdb8e719679f441b98a2251dc
Priyanshu8494 <priyanshukumar.8394pk@gmail.com>
2025-05-19 11:57:14 +0530

    Delete DownloadOffice2024.bat

--- DownloadOffice2024.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0DownloadOffice2024.ps1" -Branch "ProPlus2024Volume" -Channel "PerpetualVL2024" -Components "Word", "Excel", "PowerPoint"
-pause
